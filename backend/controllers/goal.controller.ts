3b0c06072984f40785f303bc1b4f37a5834e507a
Fenton <Fentontreasure@gmail.com>
2022-07-11 15:25:52 +0100

    🎨 Goal Rest API
    fuctional rest api running

--- backend/controllers/goal.controller.ts
+++ backend/controllers/goal.controller.ts
@@ -39,13 +39,25 @@ export const updateGoal = asyncHander(async (req: Request, res: Response) => {
     res.status(400)
     throw new Error('Goal not found')
   }
-// if parameters are passed, mongoose will update infomation 
+  // if parameters are passed, mongoose will update infomation 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
-// respond with status 200 and updated infomation 
+  // respond with status 200 and updated infomation 
   res.status(200).json(updatedGoal);
 });
 
 // @desc Delete Goal
 // @route DELETE /api/goals/:id
 // @access Private
-export const deleteGoal = asyncHander(async (req: Request, res: Response) => { res.status(200).json({ message: `DELETE Goals ${req.params.id}` }); });
+export const deleteGoal = asyncHander(async (req: Request, res: Response) => {
+  // find goal with req.params id
+  const goal = await Goal.findById(req.params.id)
+  //if the goal does not exist return status 400 with an error letting one know a goal was not found
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+//Remove Goal
+  await goal.remove()
+//Return Id of returned param
+  res.status(200).json({ id: req.params.id })
+});
